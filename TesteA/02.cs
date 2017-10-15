bfe0369d4e22d555270f8d00c0ccd3c7992b8138
ralmsdeveloper <ralms@ralms.net>
2017-10-14 22:53:01 -0300

    XXX

---
+++ TesteA/02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteA
+{
+    class ProgramX2
+    {
+        static void MainX(string[] args)
+        {
+            var a = 1;
+            var xx = 2;
+            var b = 3;
+        }
+    }
+}
